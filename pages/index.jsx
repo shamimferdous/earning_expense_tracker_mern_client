be587ef2f47f216eee517b301b5179b5bffb0677
Shamim Ferdous <shamimferdous5@gmail.com>
2020-08-29 17:22:10 +0600

    checking why env vars not working

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,28 +4,32 @@ import Passcode from '../components/Passcode';
 import App from '../components/App';
 
 //importing context api
-import {AuthContext} from '../context-api/AuthContext';
+import { AuthContext } from '../context-api/AuthContext';
 
 import { useState, useContext } from 'react';
 
 const Index = (props) => {
 
-  
-const authContext = useContext(AuthContext);
 
+  const authContext = useContext(AuthContext);
+
+  function checkEnv() {
+    return `${process.env.passcode}`
+  }
 
 
   return (
-    <> 
+    <>
 
-    <Layout>
-     
-     {/* Conditional Rendering Here */}
-     { authContext.passcode ? null : <Passcode/> }
+      <Layout>
+        <h1>Checking envs {checkEnv()}</h1>
 
-     {authContext.passcode === process.env.passcode? <App/>: null }
+        {/* Conditional Rendering Here */}
+        {authContext.passcode ? null : <Passcode />}
 
-    </Layout>
+        {authContext.passcode === process.env.passcode ? <App /> : null}
+
+      </Layout>
 
     </>
   );
